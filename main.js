0dbfde111329f702afe033233f391de014b3f9cc
Triangle717 <le717@hotmail.com>
2014-01-31 19:16:10 -0500

    Begin rewrite. I'm getting some error loading the CSS,
    and I still have to write all the JavaScript to make it more than a
    pretty face.

--- main.js
+++ main.js
@@ -1,42 +1,65 @@
-/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
-/*global define, brackets */
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define, brackets, $ */
 
-define(function () {
+/*
+    HTML Skeleton
+    Created 2014 Triangle717
+    <http://Triangle717.WordPress.com/>
+
+    Licensed under The MIT Licenses
+*/
+
+define(function (require, module) {
     "use strict";
-    /*
-    Simple extension that adds a "File > New HTML Document" menu item
-    to insert an HTML "skeleton" at cursor position
-    */
-    var CommandManager = brackets.getModule("command/CommandManager"),
-        EditorManager = brackets.getModule("editor/EditorManager"),
-        Menus = brackets.getModule("command/Menus");
+
+    // Import the required Brackets modules
+    var AppInit = brackets.getModule("utils/AppInit"),
+        //CommandManager  = brackets.getModule("command/CommandManager"),
+        Dialogs = brackets.getModule("widgets/Dialogs"),
+        //EditorManager  = brackets.getModule("editor/EditorManager"),
+        ExtensionUtils = brackets.getModule("utils/ExtensionUtils"),
+        //Menus = brackets.getModule("command/Menus"),
+
+        // Pull in the entire dialog and define the toolbar button link
+        skellyDialogHtml = require("text!mainDialog.html"),
+        toolbarButtonCode = '<a href="#" id="html-skelly-toolbar">',
+
+        // Grab our logo to display in the dialog
+        skellyLogo = require.toUrl("img/HTML-Skeleton.svg");
+
+        // The extension ID
+        // FUTURE Uncomment if keyboard shortcuts/menu is is activated
+        //EXTENSION_ID = "le717.html-skeleton";
 
 
-    function inserthtmlSkelly() {
-        /* Insert the skeleton */
-        // Assign a variable for 4 space indentation for easier construction
-        var fourSpaceIndent = "\u0020\u0020\u0020\u0020";
+    function _showSkellyDialog() {
+        /* Display the HTML Skeleton box */
 
-        // The HTML skeleton
-        var htmlSkelly = '<!DOCTYPE html>\n<html lang="">\n<head>\n' + fourSpaceIndent +
-            '<meta charset="utf-8">\n' + fourSpaceIndent +'<title></title>\n' + fourSpaceIndent +
-            '<link rel="stylesheet" href="" />' + '\n</head>\n\n<body>\n' +
-            fourSpaceIndent + '<script src=""></script>\n</body>\n</html>\n';
+        var skellyDialog = Dialogs.showModalDialogUsingTemplate(skellyDialogHtml);
+        skellyDialog.getElement().find(".close").on("click", skellyDialog.close.bind(skellyDialog));
 
-        var editor = EditorManager.getFocusedEditor();
-        if (editor) {
-            // Insert the skeleton at the current cursor position
-            var insertionPos = editor.getCursorPos();
-            editor.document.replaceRange(htmlSkelly, insertionPos);
-        }
+        // Display the logo
+        $("#html-skeleton-figure img").attr("src", skellyLogo);
     }
 
+    AppInit.appReady(function () {
+        /* Load the extension after Brackets itself has finished loading */
 
-    // Register extension in the File menu
-    var EXTENSION_ID = "le717.html-skeleton";
-    CommandManager.register("New HTML Document", EXTENSION_ID, inserthtmlSkelly);
+        // Load any required CSS
+        ExtensionUtils.loadStyleSheet(module, "css/style.css");
 
-    // Assign a keyboard shortcut
-    var theMenu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
-    theMenu.addMenuItem(EXTENSION_ID, "Ctrl-Shift-N");
+        // Add the shortcut (and only method of access) to the toolbar
+        var $toolbarButton = $(toolbarButtonCode);
+        $toolbarButton.appendTo("#main-toolbar > .buttons");
+
+        // Assign a keyboard shortcut and option in File meby
+        // FUTURE Not sure if I should do this or not
+        //CommandManager.register("Insert HTML elements", EXTENSION_ID, _showSkellyDialog);
+        //var theMenu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
+        //theMenu.addMenuItem(EXTENSION_ID, "Ctrl-Shift-N");
+
+        // Set the button's title attribute, open dialog when clicked
+        $toolbarButton.attr("title", "Insert HTML elements");
+        $toolbarButton.click(_showSkellyDialog);
+    });
 });
