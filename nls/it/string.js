9ed7ad806cf7c0e27cb7f96af1d83d9f06e885d0
Denisov21 <student@alice.it>
2014-06-30 17:12:59 +0200

    Create string.js
    Add Italian language!

---
+++ nls/it/string.js
@@ -0,0 +1,53 @@
+/*
+* Copyright (c) 2012 Adobe Systems Incorporated. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS IN THE SOFTWARE.
+*
+*/
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define */
+
+define({
+    // Menu item
+    "INSERT_HTML_ELEMENTS"         : "Inserire elementi HTML\u2026",
+
+    // mainDialog.html
+    "DIALOG_TITLE"                 : "HTML Skeleton",
+    "DIALOG_EXTENSION_DESCRIPTION" : "HTML Skeleton consente di aggiungere vari elementi HTML direttamente nel documento, senza doverli digitare.",
+    "FILE_DIALOG_TITLE"            : "Scegli un immagine",
+    "USAGE"                        : "Uso",
+    "PLACE_CURSOR"                 : "Posizionare il cursore dove si desidera aggiungere gli elementi.",
+    "SELECT_ELEMENTS"              : "Selezionare gli elementi che desiderate.",
+    "CLICK_DONE"                   : "Click su Fatto e l'elemento HTML sarà aggiunto al documento!",
+    "ELEMENTS"                     : "Elementi",
+    "HEAD_AND_BODY"                : "Head e Body con <title> e <meta>",
+    "EXTERNAL_STYLESHEET"          : "Foglio di stile CSS esterno",
+    "INLINE_STYLESHEET"            : "Foglio di stile CSS interno",
+    "EXTERNAL_SCRIPT"              : "Script esterno",
+    "INLINE_SCRIPT"                : "Script interno",
+    "PICTURE_IMAGE"                : "Immagine",
+    "WIDTH"                        : "Lunghezza",
+    "HEIGHT"                       : "Altezza",
+    "FULL_SKELETON"                : "Completa HTML Skeleton",
+    "FULL_SKELETON_DESCRIPTION"    : "Comprende tutti gli elementi sopra indicati, salvo foglio di stile interno, script interno, e immagine",
+    "BTN_CANCEL"                   : "Cancella",
+    "BTN_DONE"                     : "Fatto",
+    "BTN_BROWSE"                   : "Sfoglia\u2026",
+});
