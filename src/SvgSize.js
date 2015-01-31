e2b011766f45d1a8aaad70245837bbd8f2efc947
Triangle717 <le717.code@yahoo.com>
2015-01-30 21:25:49 -0500

    camelCase

--- src/SvgSize.js
+++ src/SvgSize.js
@@ -19,11 +19,11 @@ define(function(require, exports, module) {
   /**
    * @private
    * Get SVG graphic file object
-   * @param svgfile {string} absolute path to SVG graphic
+   * @param svgFile {string} absolute path to SVG graphic
    * @return {jQuery.Promise} promise that contains a File object
    */
-  function _readSVG(svgfile) {
-    return FileUtils.readAsText(FileSystem.getFileForPath(svgfile));
+  function _readSVG(svgFile) {
+    return FileUtils.readAsText(FileSystem.getFileForPath(svgFile));
   }
 
   /**
@@ -39,15 +39,15 @@ define(function(require, exports, module) {
   /**
    * Attempt to extract the size of an SVG graphic
    * from the width/height, viewBox and enable-background attributes
-   * @param svgfile {string} absolute path to SVG graphic
+   * @param svgFile {string} absolute path to SVG graphic
    * @return {jQuery.Promise} promise that resolves to a
    * two-index array containing the respective width and height or NaN
    * if the size could not be extracted
    */
-  function getSVGSize(svgfile) {
+  function getSVGSize(svgFile) {
     var result = new $.Deferred();
 
-    _readSVG(svgfile).then(function(content) {
+    _readSVG(svgFile).then(function(content) {
       // Add the SVG to the DOM
       var $svgContainer    = $("<div class='html-skeleton-svg'/>").css("display", "none").html(content),
           $svgElement      = $svgContainer.find("svg");
@@ -65,14 +65,18 @@ define(function(require, exports, module) {
       var backgroundWidth  = parseInt(backgroundSizes[3], 10),
           backgroundHeight = parseInt(backgroundSizes[4], 10);
 
-      // Check the validity of the extracted values,
-      // preferring width/height attributes, then viewBox values
+      // Check the validity of the extracted values
       var svgSize = [NaN, NaN];
 
+      // Width/height attributes
       if (_checkIfValid(attrWidth, attrHeight)) {
         svgSize = [attrWidth, attrHeight];
+
+        // viewBox values
       } else if (_checkIfValid(viewBoxWidth, viewBoxHeight)) {
         svgSize = [viewBoxWidth, viewBoxHeight];
+
+        // `enable-background` attribute
       } else if (_checkIfValid(backgroundWidth, backgroundHeight)) {
         svgSize = [backgroundWidth, backgroundHeight];
       }
