0f21dd5b25c208014b759f3d9b48efe3dfbb10c3
Triangle717 <le717.code@yahoo.com>
2015-01-31 15:09:06 -0500

    Configure JSCS

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,13 @@ module.exports = function(grunt) {
   "use strict";
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
+    jsFiles: ["*.js", "src/*.js"],
+    i18nJsFiles: ["nls/*.js", "nls/**/*.js"],
 
     htmlhint: {
       html: {
         options: {
-          "tag-pair": true,
+          htmlhintrc: ".htmlhintrc"
         },
         src: ["htmlContent/*.html"]
       }
@@ -27,19 +29,32 @@ module.exports = function(grunt) {
         options: {
           jshintrc: ".jshintrc"
         },
-        src: ["*.js", "src/*.js", "nls/*.js", "nls/**/*.js"],
+        src: ["<%= jsFiles %>", "<%= i18nJsFiles %>"]
       },
+    },
+
+    jscs: {
+      main: {
+        src: "<%= jsFiles %>"
+      },
+      i18n: {
+        src: "<%= i18nJsFiles %>",
+        options: {
+          maximumLineLength: null
+        }
+      },
+      options: {
+        config: ".jscs.json"
+      }
     }
   });
 
   require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
-  require("jsonlint");
-  require("jscs");
 
   grunt.registerTask("default", "List of commands", function() {
     grunt.log.writeln("");
     grunt.log.writeln("Run 'grunt lint' to lint the source files");
   });
 
-  grunt.registerTask("lint", ["htmlhint", "jshint", "csslint"]);
+  grunt.registerTask("lint", ["htmlhint", "csslint", "jshint", "jscs"]);
 };
